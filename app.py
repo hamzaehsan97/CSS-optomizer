75704fc806756344bc83861c3ddc9a33514b8f5b
hamzaehsan97 <hamzaehsan75@gmail.com>
2021-06-29 10:14:41 -0500

    rm extra files

---
+++ app.py
@@ -0,0 +1,7 @@
+from optomize.main import Optomize
+
+
+
+if __name__ == "__main__":
+    opt = Optomize("/Users/hamzaehsen/Projects/test-cases/CSS-optomizer/optomize/index.html", "/Users/hamzaehsen/Projects/test-cases/CSS-optomizer/optomize/styling.css")
+    opt.run()
